9a74833bce1b922f75a5f0521a12fe867db22598
Philipsd21 <46075925+Philipsd21@users.noreply.github.com>
2021-02-19 16:53:51 +0000

    Fixed formatting issues

--- out/production/Final/Tweet Counter.py
+++ out/production/Final/Tweet Counter.py
@@ -1,4 +1,4 @@
-# import TwitterAPI
+# from TwitterAPI import TwitterAPI
 #
 # #https://geduldig.github.io/tutorials/twitter-counter/
 #
@@ -7,10 +7,4 @@
 # ACCESS_TOKEN =
 # ACCESS_TOKEN_SECRET=
 #
-# accounts = []
-# reader = open("Twits.txt","r")
-# for line in reader:
-# accounts.append(line)
-
-
 # api = TwitterAPI(API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
