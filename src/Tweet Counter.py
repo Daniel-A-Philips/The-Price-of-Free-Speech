4369e1598fa82749ab890da613ab0f79e92f68dd
Philipsd21 <46075925+Philipsd21@users.noreply.github.com>
2021-02-21 18:21:42 +0000

    Update

--- src/Tweet Counter.py
+++ src/Tweet Counter.py
@@ -1,4 +1,5 @@
 # import TwitterAPI
+import csv
 #
 # #https://geduldig.github.io/tutorials/twitter-counter/
 #
@@ -14,3 +15,15 @@
 
 
 # api = TwitterAPI(API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+originalData = []
+with open('D:\Topics in Computer Science\Final\Data.csv',newline='') as csvfile:
+    reader = csv.reader(csvfile,delimiter=",")
+    for row in reader:
+        originalData.append(row)
+
+newData = originalData
+newData[0].append("tweets")
+
+for row in newData:
+    print(row)
