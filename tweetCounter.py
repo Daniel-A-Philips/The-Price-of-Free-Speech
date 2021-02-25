30915e35c491b6710d2f237dd53898c31d6457de
Philipsd21 <46075925+Philipsd21@users.noreply.github.com>
2021-02-25 16:31:05 +0000

    created Tweet Counter

--- tweetCounter.py
+++ tweetCounter.py
@@ -25,7 +25,6 @@ def get_params():
     # source, text, and withheld
     d = str(datetime.datetime.utcnow().isoformat("T")) # <-- get time in UTC
     end = d[0:d.index(".")] + "Z"
-    print(end)
     return {"tweet.fields": "created_at","end_time":end,"start_time":"2021-02-22T23:59:59Z","max_results":"100"}
 
 
@@ -36,8 +35,8 @@ def create_headers(bearer_token):
 
 def connect_to_endpoint(url, headers, params):
     response = requests.request("GET", url, headers=headers, params=params)
-    print(response.status_code)
     if response.status_code != 200:
+        print(response.status_code)
         raise Exception(
             "Request returned an error: {} {}".format(
                 response.status_code, response.text
@@ -53,24 +52,54 @@ def getID():
         if index == 0:
             index += 1
             continue
-        ID.append(line[1])
+        ID.append(line)
+
+def writeData(data):
+    outfile = open(writeFileName,'w')
+    outfile.write(data)
+
+def writeTotal():
+    totalTweets = 0
+    read_file = ""
+    read_data = []
+    for id in ID:
+        read_file = "User_Data//"
+        read_file += id[0]+".json"
+        read_file = open(read_file,'r')
+        for line in read_file:
+            if "result_count" in line:
+                read_data.append(line)
+    write_file = "User_Data//Admin.json"
+    write_file = open(write_file,'w')
+    index = 0
+    for line in read_data:
+        line = line[line.index(":")+2:len(line)]
+        totalTweets += int(line)
+        write_file.write(ID[index][0]+":"+ line)
+        index += 1
+    print(totalTweets,"Tweets")
 
 def main():
     global fileName
+    global writeFileName
     global ID
     global Data
+    global totalTweets
     fileName = "Data//Handles_ID.csv"
     ID = []
     Data = []
     getID()
     bearer_token = auth()
     for id in ID:
-        url = create_url(id)
+        writeFileName = "User_Data//"
+        writeFileName += id[0]+".json"
+        url = create_url(id[1])
         headers = create_headers(bearer_token)
         params = get_params()
         json_response = connect_to_endpoint(url, headers, params)
         Data.append(json_response)
-        print(json.dumps(json_response, indent=4, sort_keys=True))
+        writeData(json.dumps(json_response,indent=4,sort_keys=True))
+    writeTotal()
 
 
 if __name__ == "__main__":
