823fc88ae180873d0342a6f9edb9ccf819902593
philipsddd <46075925+philipsddd@users.noreply.github.com>
2021-03-16 10:37:10 +0000

    Update Correlation.py

--- Correlation.py
+++ Correlation.py
@@ -10,4 +10,5 @@ def histogram_intersection(a,b):
 Variance = pd.Series([.223,.324,.41,.51231])
 Tweets = pd.Series([2,32,70,43])
 print(Variance.corr(Tweets,method=histogram_intersection))
+print(Tweets.corr(Variance,method=histogram_intersection))
 
